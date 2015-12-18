02ed7c4dfd732628674f19c5077b918670d66587
Guinsly Mondesir <guinslym @gmail.com>
2015-12-17 20:23:43 -0500

    initial commit

---
+++ emplois/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+@python_2_unicode_compatible
+class Job(models.Model):
+
+    def __str__(self):
+        return self.position
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=7) <= self.pub_date <= now
+
+
+    joburl = models.URLField(max_length=250)
+    expirydate = models.DateField()
+    salarymax = models.DecimalField(max_digits=6, decimal_places=2)
+    name = models.CharField(max_length=40) 
+    position = models.CharField(max_length=150)
+    jobref = models.CharField(max_length=30)
+    job_summary = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+
+@python_2_unicode_compatible
+class Description(models.Model):
+    jobs = models.ForeignKey(Job, on_delete=models.CASCADE)
+    salarytype = models.CharField(max_length=250)
+    knowledge = models.TextField()
+    language_certificates = models.TextField()
+    educationandexp = models.TextField()
+    company_desc = models.TextField()
+
+
+
---
+++ emplois/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.utils import timezone
+
+from .models import Job, Description
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name='emplois/index.html'
+    context_object_name='latest_jobs_list'
+
+    def get_queryset(self):
+        return Job.objects.order_by('-pub_date')
+
