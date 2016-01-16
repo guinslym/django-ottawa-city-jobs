2fe287425934a99778e705cc70b50d9b60dbfe58
Guinsly Mondesir <guinslym @gmail.com>
2016-01-15 21:14:32 -0500

    add requirements for heroku

--- ottawacityjobs/settings.py
+++ ottawacityjobs/settings.py
@@ -169,11 +169,20 @@ DEFAULT_LANGUAGE = 0
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
 
 LOCALE_PATHS = ('locale', )
 
 GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-1234567-8'
 
 
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
