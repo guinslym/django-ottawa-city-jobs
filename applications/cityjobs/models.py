35cd7b36ff5f04d0f26e5f18e2cc61236179b956
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-05-09 08:37:47 -0400

    putting objects into db

--- applications/cityjobs/models.py
+++ applications/cityjobs/models.py
@@ -20,7 +20,7 @@ class Job(models.Model):
             )
     company_desc = models.TextField(blank=True, null=True)
     educationandexp = models.TextField(blank=True, null=True)
-    expirydate = models.DateField(auto_now=False, blank=True, null=True)
+    expirydate = models.CharField(max_length=150, blank=True, null=True)
     joburl = models.URLField(max_length=250, blank=True, null=True)
     jobref = models.CharField(max_length=30, unique=True, blank=True, null=True)
     job_summary = models.TextField(blank=True, null=True)
@@ -29,11 +29,11 @@ class Job(models.Model):
     language = models.CharField(max_length=2, choices=LANGUAGE_CHOICE, default=ENGLISH) 
     name = models.URLField(max_length=250, blank=True, null=True)
     position = models.CharField(max_length=150, blank=True, null=True)
-    postdate = models.DateTimeField(auto_now=False, blank=True, null=True)
+    postdate = models.CharField(max_length=150, blank=True, null=True)
     salarymin = models.CharField(max_length=40, blank=True, null=True)
     salarymax = models.CharField(max_length=40, blank=True, null=True)
     salarytype = models.CharField(max_length=20, blank=True, null=True)
-    tweeted = models.BooleanField(default=False, blank=True, null=True)
+    tweeted = models.BooleanField(default=False)
     slug = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
--- applications/cityjobs/utils.py
+++ applications/cityjobs/utils.py
@@ -1,39 +1,68 @@
 # -*- coding: utf-8 -*-
 
-from .models import Job
 import json
+import os
+from .models import Job
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #get the absolute path
 def open_json_file():
-    with open('data.json') as data_file: 
+    with open('/a.json') as data_file: 
         data = json.load(data_file)
+    for item in data['jobs']:
+        Job.objects.create(
+        company_desc = item.get("COMPANY_DESC"),
+        educationandexp = item.get("EDUCATIONANDEXP"),
+        expirydate = item.get("EXPIRYDATE"),
+        joburl = item.get("JOBURL"),
+        jobref = item.get("JOBREF"),
+        job_summary = item.get("JOB_SUMMARY"),
+        knowledge = item.get('KNOWLEDGE'),
+        language_certificates = item.get('LANGUAGE_CERTIFICATES'),
+        language  = item.get('LANGUAGE'),
+        name = item.get('NAME'),
+        position = item.get('POSITION'),
+        postdate = item.get('POSTDATE'),
+        salarymin = item.get('SALARYMIN'),
+        salarymax = item.get('SALARYMAX'),
+        salarytype = item.get('SALARYTYPE')
+    )
     return data
+
 
 
 #load json files
 
 #get each row into the db
-for item in data:
-    cdesc = item.get('')
-    eduexp = item.get('')
-    expdate =
-    jref = 
-    jurl = 
-    jsumm = 
-    know =
-    lang_cert = 
-    n = 
-    posit = 
-    pdate =
-    smax = 
-    smin = 
-    stype =
+''' shell
+https://stackoverflow.com/questions/2265357/parse-date-string-and-change-format
+
+import json
+import os
+from applications.cityjobs.models import Job 
+from applications.cityjobs.utils import open_json_file
+from ottawacityjobs.settings import BASE_DIR
+
+with open(BASE_DIR+'/a.json') as data_file: 
+    data = json.load(data_file)
+
+for item in data['jobs']:
     Job.objects.create(
-       dd
-       dd
-       dd
-       dd
-            )
-
-
-
+    company_desc = item.get("COMPANY_DESC"),
+    educationandexp = item.get("EDUCATIONANDEXP"),
+    expirydate = item.get("EXPIRYDATE"),
+    joburl = item.get("JOBURL"),
+    jobref = item.get("JOBREF"),
+    job_summary = item.get("JOB_SUMMARY"),
+    knowledge = item.get('KNOWLEDGE'),
+    language_certificates = item.get('LANGUAGE_CERTIFICATES'),
+    language  = item.get('LANGUAGE', 'EN'),
+    name = item.get('NAME'),
+    position = item.get('POSITION'),
+    postdate = item.get('POSTDATE'),
+    salarymin = item.get('SALARYMIN'),
+    salarymax = item.get('SALARYMAX'),
+    salarytype = item.get('SALARYTYPE')
+)
+'''
